9964a469ceb8058f255d2f1394a050e5442e6240
IAS <chicken.rutin@gmail.com>
2026-06-06 16:58:21 +0800

    Add files via upload

---
+++ dat.js
@@ -0,0 +1,6 @@
+let photoURL = "image/"
+const photodata = [
+    {url:`${photoURL}IMG_0006.PNG`, shotwith:'iPhone SE 1st generation', date:'February 16, 2024 3:52 PM', tag:''},
+    {url:`${photoURL}IMG_0033.PNG`, shotwith:'iPhone SE 1st generation', date:'March 30, 2025 1:26 PM', tag:''},
+    {url:`${photoURL}IMG_3294.JPG`, shotwith:'iPhone SE 2nd generation', date:'April 5, 2025 2:56 PM', tag:''},
+];
